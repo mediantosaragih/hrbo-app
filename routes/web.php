de0cbec67cdbb61b8d90e07a5867d65e8a5453aa
ifs19037 <rioefraim25@gmail.com>
2023-12-19 19:41:44 +0700

    19-12-23
    Dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,35 @@ use App\Http\Controllers\Auth\LoginRegisterController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/employee', function () {
+    return view('employee');
+});
+
+Route::get('/roster', function () {
+    return view('roster');
+});
+
+Route::get('/attendance', function () {
+    return view('attendance');
+});
+
+Route::get('/overtime', function () {
+    return view('overtime');
+});
+
+Route::get('/compensation', function () {
+    return view('compensation');
+});
+
+Route::get('/remuneration', function () {
+    return view('remuneration');
+});
+
+Route::get('/performance', function () {
+    return view('performance');
 });
 
 Route::controller(LoginRegisterController::class)->group(function() {
